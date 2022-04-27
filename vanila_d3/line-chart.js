f62f92f3e3422b953d795875a81c535dbe0636fd
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-04-27 19:23:59 +0900

    Feat: dot graph d3 line graph cant iterate data. so add circle for mouse over right data.

--- vanila_d3/line-chart.js
+++ vanila_d3/line-chart.js
@@ -14,24 +14,24 @@ const mdMargin = {top: 0.5}
 
 const mdList = [
     [
-  {date: new Date('2009-07-24 00:00:00'), value: 1},
-  {date: new Date('2009-07-29 00:00:00'), value: 2},
-  {date: new Date('2009-08-28 00:00:00'), value: 3},
-  {date: new Date('2009-12-18 00:00:00'), value: 3},
-  {date: new Date('2010-04-26 00:00:00'), value: 5},
-  {date: new Date('2010-12-08 00:00:00'), value: 2},
-  {date: new Date('2011-04-13 00:00:00'), value: 2},
-  {date: new Date('2011-08-17 00:00:00'), value: 2}
+  {date: new Date('2009-07-24 00:00:00'), value: 1, important: 1},
+  {date: new Date('2009-07-29 00:00:00'), value: 2, important: 1},
+  {date: new Date('2009-08-28 00:00:00'), value: 3, important: 1},
+  {date: new Date('2009-12-18 00:00:00'), value: 3, important: 0},
+  {date: new Date('2010-04-26 00:00:00'), value: 5, important: 1},
+  {date: new Date('2010-12-08 00:00:00'), value: 2, important: 0},
+  {date: new Date('2011-04-13 00:00:00'), value: 2, important: 0},
+  {date: new Date('2011-08-17 00:00:00'), value: 2, important: 0}
   ],
     [
-  {date: new Date('2010-02-14 00:00:00'), value: 2},
-  {date: new Date('2010-03-14 00:00:00'), value: 1},
-  {date: new Date('2010-05-09 00:00:00'), value: 1},
-  {date: new Date('2010-05-27 00:00:00'), value: 1},
-  {date: new Date('2010-08-30 00:00:00'), value: 2},
-  {date: new Date('2010-11-28 00:00:00'), value: 2},
-  {date: new Date('2011-03-27 00:00:00'), value: 2},
-  {date: new Date('2012-09-25 00:00:00'), value: 2}
+  {date: new Date('2010-02-14 00:00:00'), value: 2, important: 1},
+  {date: new Date('2010-03-14 00:00:00'), value: 1, important: 1},
+  {date: new Date('2010-05-09 00:00:00'), value: 1, important: 0},
+  {date: new Date('2010-05-27 00:00:00'), value: 1, important: 0},
+  {date: new Date('2010-08-30 00:00:00'), value: 2, important: 0},
+  {date: new Date('2010-11-28 00:00:00'), value: 2, important: 0},
+  {date: new Date('2011-03-27 00:00:00'), value: 3, important: 1},
+  {date: new Date('2012-09-25 00:00:00'), value: 4, important: 1}
   ],
 ];
 
@@ -41,75 +41,83 @@ let cur_data = 0
 
 for (const data of mdList) {
     const x = d3.scaleTime()
-  .domain([start_date, last_date])
-  .range([margin.left, width - margin.right]);
+        .domain([start_date, last_date])
+        .range([margin.left, width - margin.right]);
 
-const y = d3.scaleLinear()
-  .domain([0, d3.max(data, d => d.value)+mdMargin.top]).nice()
-  .range([height - margin.bottom, margin.top]);
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)+mdMargin.top]).nice()
+        .range([height - margin.bottom, margin.top]);
 
-const xAxis = g => g
-  .attr("transform", `translate(0,${height - margin.bottom})`)
-  .call(d3.axisBottom(x).ticks(width / 90).tickSizeOuter(0));
+    const xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x).ticks(width / 90).tickSizeOuter(0));
 
-const yAxis = g => g
-  .attr("transform", `translate(${margin.left},0)`)
-  .call(d3.axisLeft(y))
-  .call(g => {
-    return g.select(".tick:last-of-type text").clone()
-        .attr("x", 10)
-        .attr("y", 20)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .attr("font-size", '20px')
-        .text(mdLabel[cur_data])
-    });
+    const yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => {
+            return g.select(".tick:last-of-type text").clone()
+                .attr("x", 10)
+                .attr("y", 20)
+                .attr("text-anchor", "start")
+                .attr("font-weight", "bold")
+                .attr("font-size", '20px')
+                .text(mdLabel[cur_data])
+        });
 
-const line = d3.line()
-  .defined(d => !isNaN(d.value))
-  .x(d => x(d.date))
-  .y(d => y(d.value));
+    const line = d3.line()
+        .defined(d => !isNaN(d.value))
+        .x(d => x(d.date))
+        .y(d => y(d.value));
 
-var div = d3.select("body").append("div")
-     .attr("class", "tooltip-box")
-     .style("opacity", 0);
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip-box")
+        .style("opacity", 0);
 
-const svg = d3.select('body').append('svg').style('width', width).style('height', height);
-svg.append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 3)
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("d", line)
-    .on('mouseover', function (d, i) {
-      console.log(d[i].value)
-      console.log(i)
-          d3.select(this).transition()
-               .duration('50')
-               .attr('opacity', '.85');
-          div.transition()
-               .duration(50)
-               .style("opacity", 1);
-          div.html(d[i].value)
-              .style("left", (d3.event.pageX + 30) + "px")
-              .style("top", (d3.event.pageY - 30) + "px");
-     })
-   .on('mouseout', function (d, i) {
-        d3.select(this).transition()
-             .duration('50')
-             .attr('opacity', '1');
-        div.transition()
-            .duration('50')
-            .style("opacity", 0);
-     });
+    const svg = d3.select('body').append('svg').style('width', width).style('height', height);
 
-svg.append('g').call(xAxis);
-svg.append('g').call(yAxis);
-svg.node();
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 3)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("d", line)
 
-cur_data += 1
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("cx", d=>x(d.date))
+        .attr("cy", d=>y(d.value))
+        .style("fill", "black")
+        .on('mouseover', function (d) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '.85');
+            div.transition()
+                .duration(50)
+                .style("opacity", 1);
+            div.html(d.value)
+                .style("left", (d3.event.pageX + 30) + "px")
+                .style("top", (d3.event.pageY - 30) + "px");
+        })
+        .on('mouseout', function (d, i) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '1');
+            div.transition()
+                .duration('50')
+                .style("opacity", 0);
+        });
+
+    svg.append('g').call(xAxis);
+    svg.append('g').call(yAxis);
+    svg.node();
+
+    cur_data += 1
 
 }
 
