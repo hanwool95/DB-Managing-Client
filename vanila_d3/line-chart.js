ac7a6c9c5deabce9782e4e8e58b8910151bdb6ca
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-06-22 21:09:04 +0900

    Fix: Dot graph order & hover all event

--- vanila_d3/line-chart.js
+++ vanila_d3/line-chart.js
@@ -3,6 +3,17 @@ let MainData
 
 // line-chart.js
 
+const lb_filtered_condition = ["Uric Acid(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)", 'GPT (ALT)(검사24시간가능)', 'GOT (AST)(검사24시간가능)',
+    'Creatinine(검사24시간가능)', 'GOT (AST)(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)',
+    "Creatinine(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)", 'GPT (ALT)(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)']
+
+const lb_outlier_dict = {
+    "Uric Acid(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)": [3, 7],
+    'GPT (ALT)(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)': [1, 40],
+    'GOT (AST)(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)': [1, 40],
+    "Creatinine(\uac80\uc0ac24\uc2dc\uac04\uac00\ub2a5)": [0.7, 1.4]
+}
+
 let loadingMainData = (path) => {
     return fetch(path)
         .then(response => response.json())
@@ -31,90 +42,6 @@ let originMdList = []
 
 let event_bars = []
 
-// const start_date = new Date('2009-01-26 08:56:02')
-// const last_date = new Date('2012-09-05 09:40:47')
-// const originMdLabel = ['Latanoprost 0.005% 2.5ml oph', 'Benzbromarone 50mg tab']
-// const originLbLabel = ['Segmented neutrophil', 'Creatinine', 'Bilirubin, total', 'HDL-Cholesterol']
-//
-// const importantList = [
-//     {date: new Date('2009-07-24 00:00:00')},
-//     {date: new Date('2009-07-29 00:00:00')},
-//     {date: new Date('2009-08-28 00:00:00')},
-//     {date: new Date('2010-02-14 00:00:00')},
-//     {date: new Date('2010-03-14 00:00:00')},
-//     {date: new Date('2010-04-26 00:00:00')},
-//     {date: new Date('2011-03-27 00:00:00')},
-//     {date: new Date('2012-08-12 10:32:32')},
-//     {date: new Date('2012-01-12 10:32:32')},
-//     {date: new Date('2012-03-12 10:32:32')},
-// ]
-//
-// const originLbList = [
-//     [
-//         {date: new Date('2009-01-26 08:56:02'), value: 52.7},
-//         {date: new Date('2009-07-10 10:30:44'), value: 52.4},
-//         {date: new Date('2009-11-11 10:17:07'), value: 52.5},
-//         {date: new Date('2010-04-28 10:34:34'), value: 53.9},
-//         {date: new Date('2010-10-13 10:16:23'), value: 53.9},
-//         {date: new Date('2011-07-28 11:04:08'), value: 46.7},
-//         {date: new Date('2012-01-12 10:32:32'), value: 58.7},
-//         {date: new Date('2012-09-05 09:40:47'), value: 53.1}
-//     ],
-//     [
-//         {date: new Date('2009-01-26 08:56:02'), value: 0.94},
-//         {date: new Date('2009-07-10 10:30:44'), value: 1.08},
-//         {date: new Date('2009-11-11 10:17:07'), value: 1.00},
-//         {date: new Date('2010-04-28 10:34:34'), value: 0.96},
-//         {date: new Date('2010-10-13 10:16:23'), value: 1.14},
-//         {date: new Date('2011-07-28 11:04:08'), value: 1.05},
-//         {date: new Date('2012-01-12 10:32:32'), value: 2.97},
-//         {date: new Date('2012-09-05 09:40:47'), value: 1.01}
-//         ],
-//     [
-//         {date: new Date('2009-01-26 08:56:02'), value: 1.0},
-//         {date: new Date('2009-07-10 10:30:44'), value: 2.2},
-//         {date: new Date('2009-11-11 10:17:07'), value: 1.8},
-//         {date: new Date('2010-04-28 10:34:34'), value: 1.6},
-//         {date: new Date('2010-10-13 10:16:23'), value: 1.0},
-//         {date: new Date('2011-07-28 11:04:08'), value: 1.2},
-//         {date: new Date('2012-01-12 10:32:32'), value: 1.7},
-//         {date: new Date('2012-09-05 09:40:47'), value: 0.9}
-//         ],
-//     [
-//         {date: new Date('2009-01-26 08:56:02'), value: 33.0},
-//         {date: new Date('2009-07-10 10:30:44'), value: 38.0},
-//         {date: new Date('2009-11-11 10:17:07'), value: 35.0},
-//         {date: new Date('2010-04-28 10:34:34'), value: 34.0},
-//         {date: new Date('2010-10-13 10:16:23'), value: 40.0},
-//         {date: new Date('2011-07-28 11:04:08'), value: 38.0},
-//         {date: new Date('2012-01-12 10:32:32'), value: 54.0},
-//         {date: new Date('2012-09-05 09:40:47'), value: 33.0}
-//         ]
-// ]
-//
-// const originMdList = [
-//     [
-//   {date: new Date('2009-07-24 00:00:00'), value: 1},
-//   {date: new Date('2009-07-29 00:00:00'), value: 2},
-//   {date: new Date('2009-08-28 00:00:00'), value: 3},
-//   {date: new Date('2009-12-18 00:00:00'), value: 3},
-//   {date: new Date('2010-04-26 00:00:00'), value: 5},
-//   {date: new Date('2010-12-08 00:00:00'), value: 2},
-//   {date: new Date('2011-04-13 00:00:00'), value: 2},
-//   {date: new Date('2011-08-17 00:00:00'), value: 2}
-//   ],
-//     [
-//   {date: new Date('2010-02-14 00:00:00'), value: 2},
-//   {date: new Date('2010-03-14 00:00:00'), value: 1},
-//   {date: new Date('2010-05-09 00:00:00'), value: 1},
-//   {date: new Date('2010-05-27 00:00:00'), value: 1},
-//   {date: new Date('2010-08-30 00:00:00'), value: 2},
-//   {date: new Date('2010-11-28 00:00:00'), value: 2},
-//   {date: new Date('2011-03-27 00:00:00'), value: 3},
-//   {date: new Date('2012-09-25 00:00:00'), value: 4}
-//   ],
-// ];
-
 let dateToOnlyDate = (date) =>{
     let dateStringSplited = date.toString().split(" ")
     return dateStringSplited[1] + " " + dateStringSplited[2] + " " + dateStringSplited[3]
@@ -123,33 +50,23 @@ let dateToOnlyDate = (date) =>{
 let making_event_bars_by_num = () => {
     let index = 0
     const events_num = MainData.event_dates
-    console.log(events_num)
-    console.log(events_num.length)
-    console.log(MainData.events)
 
     for (const [date, values] of Object.entries(MainData.events)) {
         let cur_date = new Date(date)
-        console.log(index)
-        console.log(cur_date)
         let prev_date;
         let next_date;
         if (index === 0){
-            console.log("first")
             prev_date = cur_date
             next_date = new Date(events_num[index+1])
         }
         else if (index === events_num.length - 1){
-            console.log("last")
             prev_date = new Date(events_num[index-1])
             next_date = last_date
         }
         else {
-            console.log("middle")
             prev_date = new Date(events_num[index-1])
             next_date = new Date(events_num[index+1])
         }
-        console.log(prev_date)
-        console.log(next_date)
 
         let start_date = new Date((prev_date.getTime() + cur_date.getTime()) / 2);
         let end_date = new Date((cur_date.getTime() + next_date.getTime()) / 2);
@@ -218,10 +135,13 @@ let makingAllList = () => {
     makingLine(Selected.lbList, Selected.lbLabel, lbMargin, "lab");
     makingLine(Selected.mdList, Selected.mdLabel, mdMargin, "med");
     makingBar(event_bars);
+    makingDot(Selected.lbList, Selected.lbLabel, lbMargin, 'lab');
+    makingDot(Selected.mdList, Selected.mdLabel, mdMargin, 'med');
 }
 
 
 let makingcheckBox = (label, fieldName) => {
+    $("#div_chk").append(fieldName[0].toUpperCase()+fieldName.substring(1)+"<br>")
     label.forEach((data, index) => {
         $("#div_chk")
             .append("<span id='span_chk'><input type='checkbox' id='id_chk"+fieldName+index+"' " +
@@ -244,7 +164,7 @@ let drawLineByCheck = (labelNumber, fieldName) => {
     makingAllList();
 }
 
-let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
+let drawLineGraph = (data, dataLabel, dataMargin, svgId) => {
     const x = d3.scaleTime()
         .domain([start_date, last_date])
         .range([margin.left, width - margin.right]);
@@ -267,7 +187,7 @@ let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
                 .attr("text-anchor", "start")
                 .attr("font-weight", "bold")
                 .attr("font-size", '20px')
-                .text(dataLabel[cur_number])
+                .text(dataLabel)
         });
 
     const line = d3.line()
@@ -275,10 +195,14 @@ let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
         .x(d => x(d.date))
         .y(d => y(d.value));
 
-    var div = d3.select(chart_div_id).append("div")
-        .attr("class", "tooltip-box")
-        .style("opacity", 0);
-    const svg = d3.select(chart_div_id).append('svg').style('width', width).style('height', height);
+    // var div = d3.select(chart_div_id).append("div")
+    //     .attr("class", "tooltip-box")
+    //     .style("opacity", 0);
+    const svg = d3.select(chart_div_id)
+        .append('svg')
+        .style('width', width)
+        .style('height', height)
+        .attr("id", svgId);
 
     svg.append("path")
         .datum(data)
@@ -288,6 +212,45 @@ let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
         .attr("d", line)
+
+    if (lb_outlier_dict[dataLabel]){
+        outlier = lb_outlier_dict[dataLabel]
+        svg
+            .append("rect")
+            .attr("height", y(outlier[0])-y(outlier[1]))
+            .attr("width", width)
+            .attr("x", 0)
+            .attr("y", y(outlier[1]))
+            .attr("fill", "yellow")
+            .attr('opacity', 0.15)
+    }
+
+
+    svg.append('g').call(xAxis);
+    svg.append('g').call(yAxis);
+    svg.node();
+
+
+}
+
+const drawingDot = (data, dataLabel, dataMargin, svgId) => {
+    const x = d3.scaleTime()
+        .domain([start_date, last_date])
+        .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)+dataMargin.top]).nice()
+        .range([height - margin.bottom, margin.top]);
+
+    let div = d3.select(chart_div_id).append("div")
+        .attr("class", "tooltip-box")
+        .style("opacity", 0);
+
+    const svg = d3.select(chart_div_id).select("#"+svgId)
+        .append('svg')
+        .style('width', width)
+        .style('height', height)
+        .attr("id", svgId+"dot");
 
     svg.selectAll("circle")
         .data(data)
@@ -304,8 +267,7 @@ let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
             div.transition()
                 .duration(50)
                 .style("opacity", 1);
-            let onlyDate = dateToOnlyDate(d.date)
-            div.html(d.value+"<br>"+onlyDate)
+            div.html(d.value)
                 .style("left", (d3.event.pageX + 30) + "px")
                 .style("top", (d3.event.pageY - 30) + "px");
         })
@@ -317,21 +279,24 @@ let drawLineGraph = (data, dataLabel, dataMargin, cur_number) => {
                 .duration('50')
                 .style("opacity", 0);
         });
-
-    svg.append('g').call(xAxis);
-    svg.append('g').call(yAxis);
-    svg.node();
-
-
 }
 
+let makingDot = (dataList, dataLabel, dataMargin, title) => {
+    let cur_number = 0
+    for (const data of dataList) {
+        if (!!data) {
+            drawingDot(data, dataLabel[cur_number], dataMargin, title+cur_number.toString());
+        }
+        cur_number += 1;
+    }
+}
 
 let makingLine = (dataList, dataLabel, dataMargin, title) => {
     d3.select(chart_div_id).append("h1").text(title)
     let cur_number = 0
     for (const data of dataList) {
         if (!!data) {
-            drawLineGraph(data, dataLabel, dataMargin, cur_number);
+            drawLineGraph(data, dataLabel[cur_number], dataMargin, title+cur_number.toString());
         }
         cur_number += 1
     }
@@ -354,7 +319,6 @@ let makingBar = (data) => {
         .attr("class", "tooltip-box")
         .style("opacity", 0);
 
-    console.log(data)
 
     svgs
         .selectAll("rect")
@@ -365,7 +329,7 @@ let makingBar = (data) => {
         .attr("width", (d) => barWidth(d.start_date, d.end_date))
         .attr("x", (d) => x(d.start_date))
         .attr("y", 0)
-        .attr("fill", "orange")
+        .attr("fill", "skyblue")
         .attr('opacity', (d) => {
             if (d.important){
                 return '0.3'
@@ -376,6 +340,8 @@ let makingBar = (data) => {
         })
         .on('click', function(d) {
             d.important = !d.important;
+            svgs.selectAll("rect")
+                .dispatch('mouseout')
         })
         .on('mouseover', function (d) {
             d3.select(this).transition()
@@ -433,34 +399,46 @@ async function main(){
 
     for (const [date, values] of Object.entries(events)) {
         values.lab.forEach((data) => {
-            if (data.lab_type === "Number"){
+            if (data.lab_type === "Number") {
                 let lab_name = data.lab_name
-                let lab_num = data.lab_num
+                if (lb_filtered_condition.includes(lab_name)) {
+                    let lab_num = data.lab_num
 
-                if (originLbLabel.includes(lab_name)) {
-                    lab_index = originLbLabel.findIndex((e) => e === lab_name);
-                    originLbList[lab_index].push({date: new Date(date), value: lab_num})
-                }
-                else {
-                    originLbLabel.push(lab_name)
-                    originLbList.push([{date: new Date(date), value: lab_num}])
+                    if (originLbLabel.includes(lab_name)) {
+                        lab_index = originLbLabel.findIndex((e) => e === lab_name);
+                        originLbList[lab_index].push({date: new Date(date), value: lab_num})
+                    } else {
+                        originLbLabel.push(lab_name)
+                        originLbList.push([{date: new Date(date), value: lab_num}])
+                    }
                 }
 
+
+            }
+        })
+
+        originLbList.forEach((data) => {
+            data.sort(function (a, b) {
+                if (a.date > b.date) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            })
+        })
+
+        values.med.forEach((data) => {
+            let med_name = data.med_name_ingr
+            let prescription = data.duration
+            if (originMdLabel.includes(med_name)) {
+                med_index = originMdLabel.findIndex((e) => e === med_name);
+                originMdList[med_index].push({date: new Date(date), value: prescription})
+            } else {
+                originMdLabel.push(med_name)
+                originMdList.push([{date: new Date(date), value: prescription}])
             }
         })
     }
-
-    originLbList.forEach((data) => {
-        data.sort(function (a, b) {
-            if (a.date > b.date) {
-                return -1;
-            }
-            else{
-                return 1;
-            }
-        })
-    })
-    console.log(event_bars)
 
     makingcheckBox(originLbLabel, "lb")
     makingcheckBox(originMdLabel, "md")
